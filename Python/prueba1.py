d11e46a7b4d24c8fa9636c8d9e8e5d21e80bd140
ruespe <rubenespin84@gmail.com>
2025-09-16 19:15:31 +0200

    Add files via upload

---
+++ Python/prueba1.py
@@ -0,0 +1,12 @@
+print("HOLA")
+num1=int(input("mete un número"))
+num2=int(input("mete otro"))
+sum = num1 + num2
+print(sum)
+#print("La suma dels dos valors és: " str(sum))
+print(f"La suma será: {sum: .2f}")
+sum="Hola"
+print(type(sum))
+
+sum=5
+print(f" {sum**0.5:.2f}")
