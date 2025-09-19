9fa3f468996d2ab52e7a73eba92f0f327ea7b01b
ruespe <rubenespin84@gmail.com>
2025-09-19 16:16:35 +0200

    Va bien el git ahora

--- Python/prueba1.py
+++ Python/prueba1.py
@@ -1,3 +1,4 @@
+"""
 print("HOLA")
 num1=int(input("mete un número"))
 num2=int(input("mete otro"))
@@ -10,3 +11,38 @@ print(type(sum))
 
 sum=5
 print(f" {sum**0.5:.2f}")
+
+
+
+sum=34
+result=0
+if sum==25:
+    result= sum ** 0.5
+else:
+    result= sum
+    print(f"El resultat es {result}")
+
+    """
+"""
+
+num= int (input("Introduce un numero: "))
+if num>15:
+print("Te has pasado")
+else if num<15 :
+print("no es ese")
+else:
+print("Correcto")
+"""
+
+num= int (input("Introduce un numero: "))
+
+if num>0 and num<5:
+    print("S")
+elif num>5 and num<7:
+    print("A")
+else:
+    print("E")
+
+#mirar tutorial de listas append y remove sort y demás movida
+
+
