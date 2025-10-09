61a3665dc6087b98973d48aa3822fd6dd5fc98a2
ruespe <rubenespin84@gmail.com>
2025-10-09 20:56:32 +0200

    ah piton

---
+++ nuevaTeoria.py
@@ -0,0 +1,19 @@
+try:
+    x=2/2
+except:
+    print("No se puede dividir por cero")
+else:
+    print("La division se realizo correctamente")
+finally:
+    print("Entra encara que hi ha excepció")
+
+'''
+Escriu un programa que demani dos numeros i mostrar resultado de divisom
+
+SI usuario introduce un zero como divisor
+muestra un mensaje de error "No se puede dividir por zero"
+
+Añade un ValueError
+si l'usuario no introduce numeros enteros
+
+'''
