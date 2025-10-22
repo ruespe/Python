7cdfc3b4156a7c8277cb3ac2a95fb6facc884944
ruespe <rubenespin84@gmail.com>
2025-10-22 16:55:52 +0200

    Python ficheros casi hecho

--- ficherosTexto.py
+++ ficherosTexto.py
@@ -1,17 +1,14 @@
+# fichero = open('nombreFichero',w)
 
-#fichero = open('nombreFichero',w)
+# r lee el fichero
+# w crea fichero o lo machaca en caso de existir
+# a añadir al final del documento
+# x falla al crear si ya existe
 
-#r lee el fichero
-#w crea fichero o lo machaca en caso de existir
-#a añadir al final del documento
-#x falla al crear si ya existe
+# fitxerwrite("a\n")
+# writeline("bbbbb") escribe en la linea de abajo
 
-#fitxerwrite("a\n")
-#writeline("bbbbb") escribe en la linea de abajo
-
-"""
-"""
-
+""" """
 
 
 """
@@ -38,26 +35,71 @@
 
 """
 
-def concatena(archivo1, archivo2):
-    try:
-        with open(archivo1, 'w') as archivoCrear:
-            contenido = archivoCrear.write("Me convertiré en")
-    except IOError:
-        print("No se puede leer el archivo")
 
+# def concatena(archivo1, archivo2):
+#     try:
+#         with open(archivo2, "r") as archivoCrear:
+#             contenido = archivoCrear.read()
+#     except IOError:
+#         print("No se puede leer el archivo")
+#     except FileNotFoundError:
+#         print("No se ha encontrado el archivo bro")
+
+#     try:
+#         with open(archivo1, "a") as archivoAñadir:
+#             archivoAñadir.write(" ")
+#             archivoAñadir.write(contenido)
+#     except FileNotFoundError:
+#         print("No existe bro")
+#     except IOError:
+#         print("No funciona bro")
+
+#     with open(archivo1, "r") as archivoLeer:
+#         lectura = archivoLeer.read()
+#         print(lectura)
+
+
+# nombreArchivo1 = input("Nombre archivo1: ")
+# nombreArchivo2 = input("Nombre archivo2: ")
+
+# concatena(nombreArchivo1, nombreArchivo2)
+
+"""
+3- Crea un mètode afegir, que escrigui el contingut d'una llista en un fitxer. S'ha de fer append, el contingut original del fitxer no s'ha d'esborrar.
+
+"""
+def añadir(nombreArchivo):
+    entrada = input("Introduce los valores que quieras que posea la lista: ")
+    lista = entrada.split()
+    string = " ".join(lista)
+    #print(lista)
     try:
-        with open(archivo2, 'a') as archivoAñadir:
-            archivoAñadir.write("el rey de los Piteros")
+        with open(nombreArchivo, 'a') as archivo:
+            archivo.write(string + "\n")
     except FileNotFoundError:
-        print("No existe bro")
+        print("Archivo no encontrado")
     except IOError:
-        print("No funciona bro")
-    
-    with open(archivo1, 'r') as archivoLeer:
-            lectura = archivoLeer.read()
-            print(lectura)
+        print("Error desconocido")
 
-nombreArchivo1 = input("Nombre archivo1: ")
-nombreArchivo2 = input("Nombre archivo2: ")
+    try:
+        with open(nombreArchivo, 'r') as archivo:
+            mostrar = archivo.read()
+            print(mostrar)
+    except FileNotFoundError:
+        print("No se encuentra el archivo")
+    except IOError:
+        print("Error desconocido")
 
-concatena(nombreArchivo1,nombreArchivo2)
+
+nombreArchivo = input("Nombre archivo: ")
+añadir(nombreArchivo)
+
+"""
+4- Crea un mètode escriuPos, que escrigui una frase en un fitxer, a una posició concreta. Si la posició és incorrecta, ha de mostrar un missatge informatiu.
+
+"""
+def escribirPosicion(nombreArchivo):
+
+    try:
+        with open(nombreArchivo, 'a'):
+            nombreArchivo.read()
